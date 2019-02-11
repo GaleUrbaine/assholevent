496d657937dd02713f0dd899caf93766670d4db4
GaleUrbaine <bloody_spyke@hotmail.fr>
2019-02-11 15:59:27 +0100

    validate

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
-  belongs_to: user
+  validates :start_date, presence: true
+  validates :duration, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :location, presence: true
+  belongs_to :user
 end
